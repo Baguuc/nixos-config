8be427d930e5d4a331cb2a703dd2e2dbf2046f3b
baguuc <janzukowski@proton.me>
2025-02-09 18:31:27 +0100

    update: added flatpak

--- src/configuration.nix
+++ src/configuration.nix
@@ -64,8 +64,16 @@
         variant = "";
       };
     };
+    flatpak.enable = true;
   };
   hardware.bluetooth.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-kde
+    ];
+  };
 
   console.keyMap = "pl2"; 
   
