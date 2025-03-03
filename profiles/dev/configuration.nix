a177d16550922a2c843ff727b8c89636c0ef4c7f
baguuc <janzukowski@proton.me>
2025-03-03 17:43:45 +0100

    update: added php

--- profiles/dev/configuration.nix
+++ profiles/dev/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ../../modules/base.nix
     ../../modules/services/postgres/postgres.nix
+    ../../modules/services/lampp/lampp.nix
   ];
   home-manager.users.baguuc = import ./home.nix;
 }
